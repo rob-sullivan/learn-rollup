35c0ece6c5b96ff1c6f6b3f2d13f8c850984b823
Rob Sullivan <robertosullivan5@gmail.com>
2018-11-23 10:40:54 +0000

    Merged ProseMirror with learn-rollup.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,12 +1,13 @@
 import { EditorState } from 'prosemirror-state';
 import { EditorView } from 'prosemirror-view';
-import { Schema, DOMParser } from 'prosemirror-model';
+import { Schema, DOMParser } from 'prosemirror-model'; //DOMSerializer
 import { schema } from 'prosemirror-schema-basic';
 import { addListNodes } from 'prosemirror-schema-list';
 import { exampleSetup } from 'prosemirror-example-setup';
 
 // Mix the nodes from prosemirror-schema-list into the basic schema to
 // create a schema with list support.
+
 const mySchema = new Schema({
   nodes: addListNodes(schema.spec.nodes, 'paragraph block*', 'block'),
   marks: schema.spec.marks
@@ -18,3 +19,5 @@ window.view = new EditorView(document.querySelector('#editor'), {
     plugins: exampleSetup({ schema: mySchema })
   })
 });
+
+
